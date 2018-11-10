57e9b2824f9ba25a5b76750ae81b205c3e5253fa
tranellemaner <tranellemaner@gmail.com>
2018-11-09 23:33:55 -0500

    Complete Assignment

---
+++ script/jquery_template.js
@@ -0,0 +1,15 @@
+// jQuery must be loaded - or this code will not work
+// keep the next line at the very top of all your jQuery code
+$(document).ready(() => {
+
+
+
+
+
+
+
+
+
+
+// keep the next line at the very bottom, below all your jQuery code
+});
---
+++ script/main.js
@@ -0,0 +1,37 @@
+$(document).ready(() => {
+
+      $('#foppresenting').on('mouseenter', () => {
+        $('.bigboy').attr('src', 'images/Bluefish.jpg');
+      });
+
+      $('#commissionworking').on('mouseenter', () => {
+        $('.bigboy').attr('src', 'images/duckers.jpg');
+      });
+
+      $('#chieftalk').on('mouseenter', () => {
+        $('.bigboy').attr('src', 'images/chieftalk.jpg');
+      });
+
+      $('#conferenceroomwatching').on('mouseenter', () => {
+        $('.bigboy').attr('src', 'images/conferenceroomwatching.jpg');
+      });
+
+      $('#breakroomwatching').on('mouseenter', () => {
+        $('.bigboy').attr('src', 'images/breakroomwatching.jpg');
+      });
+
+      $('#lobbywatching').on('mouseenter', () => {
+        $('.bigboy').attr('src', 'images/lobbywatching.jpg');
+      });
+
+
+
+      $('#fishers').on('mouseenter', () => {
+              $('.fishers').attr('src', 'images/Bluefish.jpg');
+          });
+
+      $('#duckers').on('mouseenter', () => {
+              $('.fishers').attr('src', 'images/duckers.jpg');
+          });
+
+});
